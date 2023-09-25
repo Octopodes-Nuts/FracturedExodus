6cbb0ae62913b415e46501c78b3d394ea1b85e79
Isaiah Raspet <isaiahraspet@gmail.com>
2023-09-24 18:39:52 -0700

    Project Setup

--- src/behavior/settings.gd
+++ src/behavior/settings.gd
@@ -3,39 +3,39 @@ extends Node
 
 # setup
 func _ready():
-    # Register Inputs
-    _set_actions()
-    _load_bindings()
+	# Register Inputs
+	_set_actions()
+	_load_bindings()
 
 # Gameplay settings
 var FOV: int
 
 var actions = [
-    "move_forward",
-    "move_backward",
-    "move_left",
-    "move_right",
-    "interact",
-    "fire",
-    "jump",
-    "exit"
+	"move_forward",
+	"move_backward",
+	"move_left",
+	"move_right",
+	"interact",
+	"fire",
+	"jump",
+	"exit"
 ]
 
 # Key Bindings
 func _set_actions():
-    for action in actions:
-        InputMap.add_action(action)
+	for action in actions:
+		InputMap.add_action(action)
 
 # Load Key Bindings from .json
 func _load_bindings():
-    InputMap.action_add_event("move_forward", InputEvent.KEY_W)
-    InputMap.action_add_event("move_backward", InputEvent.KEY_S)
-    InputMap.action_add_event("move_left", InputEvent.KEY_A)
-    InputMap.action_add_event("move_right", InputEvent.KEY_D)
-    InputMap.action_add_event("interact", InputEvent.KEY_E)
-    InputMap.action_add_event("fire", InputEventMouseButton.BUTTON_LEFT)
-    InputMap.action_add_event("jump", InputEvent.KEY_SPACE)
-    InputMap.action_add_event("exit", InputEvent.KEY_ESCAPE)
+	InputMap.action_add_event("move_forward", InputEvent.KEY_W)
+	InputMap.action_add_event("move_backward", InputEvent.KEY_S)
+	InputMap.action_add_event("move_left", InputEvent.KEY_A)
+	InputMap.action_add_event("move_right", InputEvent.KEY_D)
+	InputMap.action_add_event("interact", InputEvent.KEY_E)
+	InputMap.action_add_event("fire", InputEventMouseButton.BUTTON_LEFT)
+	InputMap.action_add_event("jump", InputEvent.KEY_SPACE)
+	InputMap.action_add_event("exit", InputEvent.KEY_ESCAPE)
 
 # For now, all events are set to defaults
 
