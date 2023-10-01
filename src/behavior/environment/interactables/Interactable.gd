a0c2db59d9d03771c3e47b11e2cfe262dec80524
Isaiah Raspet <isaiahraspet@gmail.com>
2023-09-30 21:55:41 -0700

    this implementaion of interactables is broken

--- src/behavior/environment/interactables/Interactable.gd
+++ src/behavior/environment/interactables/Interactable.gd
@@ -4,8 +4,18 @@ class_name Interactable
 
 onready var Global = get_node('/root/Global')
 
+onready var interact_zone: Area = $area # specifies the area where interaction is possible
+
+var interactable: bool = false
+
+func _physics_process(_delta):
+	if interactable and Input.is_action_just_pressed("interact"):
+		_interact()
+
+# what to do upon interaction with object
 func _interact():
 	pass
 
+# report interaction to UI, includes optional graphic or words?
 func _ui_report():
 	pass
