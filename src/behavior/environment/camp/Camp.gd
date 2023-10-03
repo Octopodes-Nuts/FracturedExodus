ac439424fb7faca588d4a1249ab1cdb17389f194
Isaiah Raspet <isaiahraspet@gmail.com>
2023-10-02 21:28:34 -0700

    lay down ground work for camp implementation

--- src/behavior/environment/camp/Camp.gd
+++ src/behavior/environment/camp/Camp.gd
@@ -1,5 +1,23 @@
-extends Spatial
+extends Area
+
+# Description of what a camp should do or be
+
+# 1. Camp needs to spawn enemies and register itself as their home
+# 	a. This is more related to AI, but AI should return to camp, maybe let them
+#	know that they have return when they enter
+
+# This should change in the future to harbor different kinds of AI
+# There may also be a need for camp types
+var number_fractured: int
+var max_fractured: int
+var fractured: Array
 
 # Placeholder for camp behavior
 func _ready():
+	# define random number of fractured
+	# mint random number of fractured
+	for ai in range(number_fractured):
+		# add as child of the camp
+		# set home camp
+		pass
 	pass # Replace with function body.
