6d8a6d46069e640bbaa0058a8bdedd4fed05d970
Isaiah Raspet <isaiahraspet@gmail.com>
2023-10-16 18:57:07 -0700

    add rudimentary extract site

--- src/behavior/environment/extract/Extract.gd
+++ src/behavior/environment/extract/Extract.gd
@@ -1,8 +1,28 @@
-extends Area
-
-onready var Global = get_node('/root/Global')
+extends Interactable
 
 export var faction: int
 
-func _interact():
-    pass
+export var EXTRACT_TIME: float = 5.0
+var current_extract_time: float = 0.0
+
+var extracts: Dictionary = {}
+
+func _init():
+	auto_interact = true
+
+func _local_physics_step(delta):
+	for node in extracts.keys():
+		if extracts[node] > 0:
+			extracts[node] -= delta
+		else:
+			node.extract()
+			#warning-ignore:return_value_discarded
+			extracts.erase(node)
+
+func _interact(node: Node):
+	# check if factions match. fcations are not yet implemented
+	extracts[node] = EXTRACT_TIME
+
+func _remove_interaction(node: Node):
+	#warning-ignore:return_value_discarded
+	extracts.erase(node)
--- src/behavior/environment/interactables/Interactable.gd
+++ src/behavior/environment/interactables/Interactable.gd
@@ -1,4 +1,4 @@
-extends Spatial
+extends Area
 
 class_name Interactable
 
@@ -7,15 +7,38 @@ onready var Global = get_node('/root/Global')
 onready var interact_zone: Area = $area # specifies the area where interaction is possible
 
 var interactable: bool = false
+var auto_interact: bool = false
+var interaction_in_process = false
 
 func _physics_process(_delta):
-	if interactable and Input.is_action_just_pressed("interact"):
-		_interact()
+		_local_physics_step(_delta)
 
 # what to do upon interaction with object
-func _interact():
+func _interact(_other: Node):
 	pass
 
 # report interaction to UI, includes optional graphic or words?
 func _ui_report():
+	pass
+
+func _local_physics_step(_delta):
+	pass
+
+# this needs to be rethought ... it must be confirmed that
+# a player that is able to interact is interacting
+# the way that it will work will need to be refactored to work over
+# the network. This will be done by confirming the player id
+# of the one who pressed the interact button with the one that is able to interact, 
+# or by handlng this interaction on the player side. May be best to do it on the player side
+func _on_interactable_body_entered(body:Node):
+	if body.has_method('register_interaction'):
+		body.register_interaction(self)
+	pass # Replace with function body.
+
+func _on_interactable_body_exited(body):
+	if body.has_method('remove_interaction'):
+		body.remove_interaction(self)
+		_remove_interaction(body)
+
+func _remove_interaction(_node: Node):
 	pass
