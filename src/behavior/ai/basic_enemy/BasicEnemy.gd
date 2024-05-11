ea9eed3091fd1ea87bca263eed822bf45c9e13f2
Isaiah Raspet <isaiahraspet@gmail.com>
2024-05-11 14:02:38 -0700

    add scan state

--- src/behavior/ai/basic_enemy/BasicEnemy.gd
+++ src/behavior/ai/basic_enemy/BasicEnemy.gd
@@ -38,8 +38,10 @@ var Awareness: AwarenessState = AwarenessState.IDLE
 # on the noise function, the creator of the noise is assigned as the target and 
 # the ai should turn to look at the player and commence chase
 func noise(direction: Vector3):
-	Awareness = AwarenessState.AWARE
-	update_target_location(direction)
+	if Awareness != AwarenessState.PERSUIT or \
+		Awareness != AwarenessState.RETREAT:
+		Awareness = AwarenessState.AWARE
+		update_target_location(direction)
 
 func _ready():
 	pass # Replace with function body.
@@ -53,17 +55,20 @@ func hit(damage: float):
 func _die():
 	queue_free()
 
+@export var scan_time: float = 1.5
+@export var current_scan_time: float = 0.0
+
 # This step is to evaluate what the enemy can see
 # And make a decision as to its current state
-func evaluate():
+func evaluate(delta):
 	if Awareness == AwarenessState.IDLE:
 		# find a random position to move to
 		randomize()
-		var positive_x = randf_range(10.0, 30.0)
-		var negative_x = randf_range(-30.0, -10.0)
+		var positive_x = randf_range(5.0, 10.0)
+		var negative_x = randf_range(-10.0, -5.0)
 		
-		var positive_y = randf_range(10.0, 30.0)
-		var negative_y = randf_range(-30.0, -10.0)
+		var positive_y = randf_range(5.0, 10.0)
+		var negative_y = randf_range(-10.0, -5.0)
 		
 		var x = 0
 		if abs(negative_x) > positive_x:
@@ -84,40 +89,46 @@ func evaluate():
 		
 		return (navigation.get_next_path_position() - global_transform.origin).normalized()
 
-	if Awareness == AwarenessState.PATROL:
+	elif Awareness == AwarenessState.PATROL:
 		# continue along current path
+		# patrolling should take place within a certain range of the AI's home location
 		if navigation.distance_to_target() < 2.0:
-			Awareness = AwarenessState.IDLE
-			print(navigation.target_position)
-			print(global_transform.origin)
+			current_scan_time = scan_time
+			Awareness = AwarenessState.SCAN
 		else:
 			return (navigation.get_next_path_position() - global_transform.origin).normalized()
 
-	if Awareness == AwarenessState.AWARE:
+	elif Awareness == AwarenessState.SCAN:
+		current_scan_time -= delta
+		if current_scan_time <= 0:
+			Awareness = AwarenessState.IDLE
+		
+		return Vector3.ZERO
+
+	elif Awareness == AwarenessState.AWARE:
 		
 		if navigation.distance_to_target() < 2.0:
 			Awareness = AwarenessState.IDLE
 		else:
 			return (navigation.get_next_path_position() - global_transform.origin).normalized()
 
-	if Awareness == AwarenessState.PERSUIT:
+	elif Awareness == AwarenessState.PERSUIT:
 		# actively chase player
 		pass
-	if Awareness == AwarenessState.RETREAT:
+	elif Awareness == AwarenessState.RETREAT:
 		# run away from player
 		pass
 		
 	return Vector3.ZERO
 
 func _physics_process(delta):
-	var current_location = global_transform.origin
 	var heading: Vector3 = Vector3.ZERO
 
 	# this can be sped up significantly, this calculation does not have to be done every frame
 	# if navigation.target_position != Vector3.ZERO or not navigation.target_reached:
 	# 	heading = (navigation.get_next_path_position() - current_location).normalized() * speed * delta
 
-	heading = evaluate() * speed * delta
+	heading = evaluate(delta) * speed * delta
 
 	if not is_on_floor():
 		gravity_direction += Vector3.DOWN * gravity * delta
