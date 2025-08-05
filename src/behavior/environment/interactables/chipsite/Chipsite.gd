8b012aff6bace43e485190c9bc932a61aec16961
Isaiah Raspet <isaiahraspet@gmail.com>
2025-08-05 11:51:12 -0700

    Add initial chipsite

--- src/behavior/environment/interactables/chipsite/Chipsite.gd
+++ src/behavior/environment/interactables/chipsite/Chipsite.gd
@@ -5,9 +5,18 @@
 
 extends Interactable
 
+const OBTAIN_TIME_TARGET = 35.0
+
+var obtain_time = 0.0
+var extracted = false
+
 func _ready():
 	Global.add_chipsite(self)
 
 # Start extraction of resource
-func interact():
-	pass
+func interact(other):
+	if not extracted:
+		obtain_time += other.delta
+		print("Obtain Time: " + str(obtain_time))
+		if obtain_time >= OBTAIN_TIME_TARGET:
+			print("Chip Extracted")
