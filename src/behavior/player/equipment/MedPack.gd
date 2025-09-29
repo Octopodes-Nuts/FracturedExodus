0399ae500f37f0ce89e6f5dad339da8192ec31c9
Isaiah Raspet <isaiahraspet@gmail.com>
2025-09-29 12:25:18 -0700

    Replace timer with delta

--- src/behavior/player/equipment/MedPack.gd
+++ src/behavior/player/equipment/MedPack.gd
@@ -10,12 +10,9 @@ class_name  MedPack
 @export var heal_rate: int = 15
 @export var health_pool: int = 150
 
-func _init():
-	continuous_usage = true
-
 func use(player: DefaultController):
 	if player.current_health != player.FULL_HEALTH and health_pool != 0:
-		var heal_amount: int = heal_rate
+		var heal_amount: int = heal_rate * player.delta
 		
 		# Do not heal past maximum health or more than available in health pool
 		if player.current_health + heal_amount > player.FULL_HEALTH:
@@ -29,11 +26,3 @@ func use(player: DefaultController):
 		print('healed ' + str(heal_amount) + ' to: '\
 		 	+ str(player.current_health))
 		print('health available: ' + str(health_pool))
-		
-		# Initiate cooldown
-		cool_down = true
-		$Timer.start()
-
-# End cooldown
-func _on_timer_timeout() -> void:
-	cool_down =  false
