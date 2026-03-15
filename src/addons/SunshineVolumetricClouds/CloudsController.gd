7bb17548918214938a0cdd4608e4121efd046fd2
Isaiah Raspet <isaiahrsaspet@gmail.com>
2026-03-14 22:32:04 -0700

    remove clouds system

--- src/addons/SunshineVolumetricClouds/CloudsController.gd
+++ src/addons/SunshineVolumetricClouds/CloudsController.gd
@@ -36,58 +36,46 @@ class_name CloudsController;
 @export var fogColorDefault : Color = Color(1, 1, 1);
 
 func _ready():
-	if (OS.has_feature("dedicated_server") || DisplayServer.get_name() == "headless"):
-		return;
-
 	if (!Engine.is_editor_hint()):
 		AddShaderVariables();
 		UpdateGlobalVariableTextures();
 		UpdateGlobalVariables();
 
-func _has_shader_global(name: String) -> bool:
-	return RenderingServer.global_shader_parameter_get_list().has(StringName(name));
-
-func _add_shader_global(name: String, var_type: int, value) -> void:
-	if (_has_shader_global(name)):
-		RenderingServer.global_shader_parameter_set(name, value);
-		return;
-	RenderingServer.global_shader_parameter_add(name, var_type, value);
-
 func AddShaderVariables():
 	var HeightWeightGradient = ResourceLoader.load("res://addons/SunshineVolumetricClouds/HeightWeightGradient.tres");
-	_add_shader_global("SunshineClouds_HeightWeightGradient", RenderingServer.GLOBAL_VAR_TYPE_SAMPLER2D, HeightWeightGradient);
+	RenderingServer.global_shader_parameter_add("SunshineClouds_HeightWeightGradient", RenderingServer.GLOBAL_VAR_TYPE_SAMPLER2D, HeightWeightGradient);
 
 	var BaseNoiseTexture = ResourceLoader.load("res://addons/SunshineVolumetricClouds/BaseNoiseTexture.tres");
 
-	_add_shader_global("SunshineClouds_BaseNoiseTexture", RenderingServer.GLOBAL_VAR_TYPE_SAMPLER3D, BaseNoiseTexture);
+	RenderingServer.global_shader_parameter_add("SunshineClouds_BaseNoiseTexture", RenderingServer.GLOBAL_VAR_TYPE_SAMPLER3D, BaseNoiseTexture);
 
 	var DetailNoiseTexture = ResourceLoader.load("res://addons/SunshineVolumetricClouds/BaseNoiseDetailTexture.tres");
 
-	_add_shader_global("SunshineClouds_DetailNoiseTexture", RenderingServer.GLOBAL_VAR_TYPE_SAMPLER3D, DetailNoiseTexture);
+	RenderingServer.global_shader_parameter_add("SunshineClouds_DetailNoiseTexture", RenderingServer.GLOBAL_VAR_TYPE_SAMPLER3D, DetailNoiseTexture);
 
 	var LargeScaleNoiseTexture = ResourceLoader.load("res://addons/SunshineVolumetricClouds/BaseNoiseLargeScaleTexture.tres");
 
-	_add_shader_global("SunshineClouds_LargeScaleNoiseTexture", RenderingServer.GLOBAL_VAR_TYPE_SAMPLER3D, LargeScaleNoiseTexture);
+	RenderingServer.global_shader_parameter_add("SunshineClouds_LargeScaleNoiseTexture", RenderingServer.GLOBAL_VAR_TYPE_SAMPLER3D, LargeScaleNoiseTexture);
 
-	_add_shader_global("SunshineClouds_SunDirection", RenderingServer.GLOBAL_VAR_TYPE_VEC3, Vector3.UP);
-	_add_shader_global("SunshineClouds_SunColor", RenderingServer.GLOBAL_VAR_TYPE_COLOR, Color(1, 1, 1));
-	_add_shader_global("SunshineClouds_FogColor", RenderingServer.GLOBAL_VAR_TYPE_COLOR, Color(1, 1, 1));
-	_add_shader_global("SunshineClouds_AmbientColor", RenderingServer.GLOBAL_VAR_TYPE_COLOR, Color(0, 0, 0));
+	RenderingServer.global_shader_parameter_add("SunshineClouds_SunDirection", RenderingServer.GLOBAL_VAR_TYPE_VEC3, Vector3.UP);
+	RenderingServer.global_shader_parameter_add("SunshineClouds_SunColor", RenderingServer.GLOBAL_VAR_TYPE_COLOR, Color(1, 1, 1));
+	RenderingServer.global_shader_parameter_add("SunshineClouds_FogColor", RenderingServer.GLOBAL_VAR_TYPE_COLOR, Color(1, 1, 1));
+	RenderingServer.global_shader_parameter_add("SunshineClouds_AmbientColor", RenderingServer.GLOBAL_VAR_TYPE_COLOR, Color(0, 0, 0));
 
-	_add_shader_global("SunshineClouds_UseFog", RenderingServer.GLOBAL_VAR_TYPE_BOOL, true);
+	RenderingServer.global_shader_parameter_add("SunshineClouds_UseFog", RenderingServer.GLOBAL_VAR_TYPE_BOOL, true);
 
-	_add_shader_global("SunshineClouds_WindDirection", RenderingServer.GLOBAL_VAR_TYPE_VEC2, Vector2.ZERO);
-	_add_shader_global("SunshineClouds_WindSpeed", RenderingServer.GLOBAL_VAR_TYPE_FLOAT, 0.003);
-	_add_shader_global("SunshineClouds_CloudsGlobalScale", RenderingServer.GLOBAL_VAR_TYPE_FLOAT, 10000.0);
-	_add_shader_global("SunshineClouds_CloudsDetailNoiseScale", RenderingServer.GLOBAL_VAR_TYPE_FLOAT, 5.921);
-	_add_shader_global("SunshineClouds_CloudsDetailNoisePower", RenderingServer.GLOBAL_VAR_TYPE_FLOAT, 1.048);
-	_add_shader_global("SunshineClouds_CloudsLargeScaleNoiseScale", RenderingServer.GLOBAL_VAR_TYPE_FLOAT, 0.216);
-	_add_shader_global("SunshineClouds_CloudsLargeScaleNoisePower", RenderingServer.GLOBAL_VAR_TYPE_FLOAT, 3.435);
-	_add_shader_global("SunshineClouds_CloudsBaseNoiseScale", RenderingServer.GLOBAL_VAR_TYPE_FLOAT, 1.761);
-	_add_shader_global("SunshineClouds_CloudsCutoff", RenderingServer.GLOBAL_VAR_TYPE_FLOAT, 0.213);
+	RenderingServer.global_shader_parameter_add("SunshineClouds_WindDirection", RenderingServer.GLOBAL_VAR_TYPE_VEC2, Vector2.ZERO);
+	RenderingServer.global_shader_parameter_add("SunshineClouds_WindSpeed", RenderingServer.GLOBAL_VAR_TYPE_FLOAT, 0.003);
+	RenderingServer.global_shader_parameter_add("SunshineClouds_CloudsGlobalScale", RenderingServer.GLOBAL_VAR_TYPE_FLOAT, 10000.0);
+	RenderingServer.global_shader_parameter_add("SunshineClouds_CloudsDetailNoiseScale", RenderingServer.GLOBAL_VAR_TYPE_FLOAT, 5.921);
+	RenderingServer.global_shader_parameter_add("SunshineClouds_CloudsDetailNoisePower", RenderingServer.GLOBAL_VAR_TYPE_FLOAT, 1.048);
+	RenderingServer.global_shader_parameter_add("SunshineClouds_CloudsLargeScaleNoiseScale", RenderingServer.GLOBAL_VAR_TYPE_FLOAT, 0.216);
+	RenderingServer.global_shader_parameter_add("SunshineClouds_CloudsLargeScaleNoisePower", RenderingServer.GLOBAL_VAR_TYPE_FLOAT, 3.435);
+	RenderingServer.global_shader_parameter_add("SunshineClouds_CloudsBaseNoiseScale", RenderingServer.GLOBAL_VAR_TYPE_FLOAT, 1.761);
+	RenderingServer.global_shader_parameter_add("SunshineClouds_CloudsCutoff", RenderingServer.GLOBAL_VAR_TYPE_FLOAT, 0.213);
 
-	_add_shader_global("SunshineClouds_CloudsFloor", RenderingServer.GLOBAL_VAR_TYPE_FLOAT, 80.0);
-	_add_shader_global("SunshineClouds_CloudsCeiling", RenderingServer.GLOBAL_VAR_TYPE_FLOAT, 2000.0);
+	RenderingServer.global_shader_parameter_add("SunshineClouds_CloudsFloor", RenderingServer.GLOBAL_VAR_TYPE_FLOAT, 80.0);
+	RenderingServer.global_shader_parameter_add("SunshineClouds_CloudsCeiling", RenderingServer.GLOBAL_VAR_TYPE_FLOAT, 2000.0);
 
 func _process(delta):
 	if (Engine.is_editor_hint() || updateConstantly):
@@ -96,32 +84,16 @@ func _process(delta):
 
 func UpdateGlobalVariableTextures():
 	if (gradientControlTexture == null):
-		var loaded_gradient = ResourceLoader.load("res://addons/SunshineVolumetricClouds/HeightWeightGradient.tres");
-		if (loaded_gradient is GradientTexture1D):
-			gradientControlTexture = loaded_gradient;
-		else:
-			gradientControlTexture = GradientTexture1D.new();
+		gradientControlTexture = ResourceLoader.load("res://addons/SunshineVolumetricClouds/HeightWeightGradient.tres");
 	
 	if (baseNoiseTexture == null):
-		var loaded_base_noise = ResourceLoader.load("res://addons/SunshineVolumetricClouds/BaseNoiseTexture.tres");
-		if (loaded_base_noise is NoiseTexture3D):
-			baseNoiseTexture = loaded_base_noise;
-		else:
-			baseNoiseTexture = NoiseTexture3D.new();
+		baseNoiseTexture = ResourceLoader.load("res://addons/SunshineVolumetricClouds/BaseNoiseTexture.tres");
 	
 	if (detailNoiseTexture == null):
-		var loaded_detail_noise = ResourceLoader.load("res://addons/SunshineVolumetricClouds/BaseNoiseDetailTexture.tres");
-		if (loaded_detail_noise is NoiseTexture3D):
-			detailNoiseTexture = loaded_detail_noise;
-		else:
-			detailNoiseTexture = NoiseTexture3D.new();
+		detailNoiseTexture = ResourceLoader.load("res://addons/SunshineVolumetricClouds/BaseNoiseDetailTexture.tres");
 	
 	if (largeScaleNoiseTexture == null):
-		var loaded_large_scale_noise = ResourceLoader.load("res://addons/SunshineVolumetricClouds/BaseNoiseLargeScaleTexture.tres");
-		if (loaded_large_scale_noise is NoiseTexture3D):
-			largeScaleNoiseTexture = loaded_large_scale_noise;
-		else:
-			largeScaleNoiseTexture = NoiseTexture3D.new();
+		largeScaleNoiseTexture = ResourceLoader.load("res://addons/SunshineVolumetricClouds/BaseNoiseLargeScaleTexture.tres");
 	
 
 func UpdateGlobalVariables():
