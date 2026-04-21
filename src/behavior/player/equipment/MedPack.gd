a2ab3a74884c1fc64c0be6492e271b48f36d2af9
Isaiah Raspet <isaiahraspet@gmail.com>
2026-04-21 13:21:12 -0700

    tune medkit to have a rampup timer and slower healing time

--- src/behavior/player/equipment/MedPack.gd
+++ src/behavior/player/equipment/MedPack.gd
@@ -10,11 +10,25 @@ class_name  MedPack
 @export var heal_rate: float = 15
 @export var health_pool: float = 150
 
+@export var pre_start = 0.5
+
+var used_this_frame: bool = false
+var _ramp_timer: float = 0.0
+
 func _init():
 	continuous_usage = true
 
+func _process(delta):
+	if not Input.is_action_pressed("fire"):
+		_ramp_timer = 0.0
+
 func use(controller: DefaultController):
+
 	if controller.current_health >= controller.FULL_HEALTH or health_pool <= 0:
+		return
+
+	_ramp_timer += controller.delta
+	if _ramp_timer < pre_start:
 		return
 
 	var requested_heal: float = heal_rate * controller.delta
